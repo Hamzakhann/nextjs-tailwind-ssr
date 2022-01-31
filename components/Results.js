4638824460234f83137aaf4488f5840f99dddbb3
Hamzakhann <hamzakhann66@gmail.com>
2022-01-31 17:07:04 +0500

    added TMDB API

--- components/Results.js
+++ components/Results.js
@@ -1,7 +1,14 @@
-import React from 'react';
-
-const Results = () => {
-  return <div>hello</div>;
+import React from "react";
+import Thumbnail from "./Thumbnail";
+import FlipMove from "react-flip-move";
+const Results = ({ results }) => {
+  return (
+    <div className="px-5 py-10 sm:grid md:grid-cols-2 lg:grid-cols-3 3xl:flex flex-wrap justify-center">
+      {results.map((item, i) => {
+        return <Thumbnail result={item} key={i} />;
+      })}
+    </div>
+  );
 };
 
 export default Results;
--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,10 @@ import Head from "next/head";
 import Header from "../components/Header";
 import Nav from "../components/Nav";
 import Results from "../components/Results";
+import requests from "../utils/requests";
 
-export default function Home() {
+export default function Home({results}) {
+  console.log(results , "......................")
   return (
     <div>
       <Head>
@@ -12,9 +14,23 @@ export default function Home() {
       </Head>
       <Header />
       <Nav />
-      <Results />
+      <Results results={results} />
     </div>
   );
 }
 
-export async function getServerSideRender(context) {}
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+  const request = await fetch(
+    `https://api.themoviedb.org/3${
+      requests[genre]?.url || requests.fetchTrending?.url
+    }`
+  )
+    .then((res) => res.json())
+    .then();
+  return {
+    props: {
+      results: request.results
+    }
+  };
+}
